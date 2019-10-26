e9948e423cb6caed0496b7c64a73c20e33e9dcaa
Skyler <sackerman@ucmerced.edu>
2019-10-26 11:08:57 -0700

    Make a list of assignments due in the future

--- canvasAPI.py
+++ canvasAPI.py
@@ -1,29 +1,38 @@
-from canvasapi import Canvas
+from canvasapi import Canvas, course, assignment
 import canvasapi
 from parse import *
 from datetime import datetime
 from twilio.rest import Client
+
+
 
 def main():
 	token = '13171~dvg11q0NmH6ZsUUXWEwbqSVjqS5cr5zchyTz49ZiXtn8lKqbt1xRhQgQ2n45YyaL'
 	url = 'https://ucsd.instructure.com/api/v1/users/self'
 	canvas = Canvas(url, token)
 	user = canvas.get_current_user()
+	print(user.id)
 
+	assignmentList = []
 	for assign in getAssignments(canvas):
-		print(assign.due_at)
+		if(not isPast(getDueDate(assign))):
+			assignmentList.append(assign)
+
+	for assign in assignmentList:
+		print(assign)
+
 	# Useful commands:
 	# canvas.get_courses(); Gets list of all courses for the user
 	# course.get_assignments(); Gets a list of all get_assignment
 	# assignment.due_at; Gets due date of assignment
 
 
-def courseHasEnded(course: canvasapi.Course):
+
+def courseHasEnded(crse):
 	return course.end_at_date.replace(tzinfo=None) < datetime.now(None)
 
 
-
-def getClassMates(canvas: Cavnvas):
+def getClassMates(canvas: Canvas):
 	temp = []
 	for course in canvas.get_courses():
 		for user in course.get_users():
@@ -37,16 +46,20 @@ def getAssignments(canvas: Canvas):
 			temp.append(assign)
 	return temp
 
-def getDueDate(assignment: canvasapi.Assignment):
+def getDueDate(assignment):
 	if assignment.due_at is not None:
 		return datetime.strptime(assignment.due_at, "%Y-%m-%dT%H:%M:%SZ")
 	else:
-		return datetime.now()
+		return datetime(1,1,1)
+
+def getDate(dateStr):
+	if dateStr:
+		return datetime.strptime(dateStr, "%Y-%m-%dT%H:%M:%SZ")
 
 def isPast(datetimeObj):
-	if(datetimeObj-datetime.now()<=0):
-		return true
-	return false
+	if(datetimeObj<datetime.now()):
+		return True
+	return False
 
 main()
 

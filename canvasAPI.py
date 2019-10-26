d354b0bfe4045a28abc5dd5011883c67d798e72d
Skyler Ackerman <skyler1ackerman@gmail.com>
2019-10-25 17:35:01 -0700

    Just a bunch of crap

--- canvasAPI.py
+++ canvasAPI.py
@@ -1,13 +1,22 @@
 from canvasapi import Canvas
+from parse import *
+from datetime import datetime
 
 def main():
 	token = '13171~dvg11q0NmH6ZsUUXWEwbqSVjqS5cr5zchyTz49ZiXtn8lKqbt1xRhQgQ2n45YyaL'
 	url = 'https://ucsd.instructure.com/api/v1/users/self'
 	canvas = Canvas(url, token)
 	user = canvas.get_current_user()
+	temp = []
 	for course in canvas.get_courses():
 		for assign in course.get_assignments():
-			print(assign.muted)
+			temp.append(getDueDate(assign))
+			print(assign, 'due in', getDueDate(assign)-datetime.now())
+			myTemp = assign.due_at
+	#print(temp)
+
+
+
 	# print(getAssignments(canvas)[0]['id'])
 # for gen in user.get_profile():
 # 	print(gen)
@@ -31,5 +40,11 @@ def getAssignments(canvas):
 		for assign in course.get_assignments():
 			temp.append(assign)
 	return temp
+
+def getDueDate(assignment):
+	if assignment.due_at is not None:
+		return datetime.strptime(assignment.due_at, "%Y-%m-%dT%H:%M:%SZ")
+	else:
+		return datetime.now()
 
 main()
